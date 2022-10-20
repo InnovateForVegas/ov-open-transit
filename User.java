ae7a1e669d37822d1d97faf2ff60b1f44bdb462e
Roger Mayo <rogermayogit@users.noreply.github.com>
2022-10-20 07:23:53 -0700

    Update User.java
    Removed status method and moved it to Route class

--- User.java
+++ User.java
@@ -1,22 +1,10 @@
 public class User
 {
-    public String status(Route route)
+    private Route route;
+    
+    public User()
     {
-        if (route.isDelayed())
-        {
-            return new String("Delayed");
-        }
-        else if (route.isCanceled())
-        {
-            return new String("Canceled");
-        }
-        else if (route.isUnavailable())
-        {
-            return new String("Unavailable");
-        }
-        else
-        {
-            return new String("On Time, on Route");
-        }
+        route = new Route();    
     }
+    
 }
